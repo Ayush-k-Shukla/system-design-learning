3a413aeae99afe000ec8c1441101b17fd8d4485d
Ayush Kumar Shukla <shuklaayushkumar27@gmail.com>
2025-07-05 20:07:37 +0530

    feat: add global search support

--- webapp/docusaurus.config.ts
+++ webapp/docusaurus.config.ts
@@ -14,6 +14,8 @@ const config: Config = {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
+  plugins: [require.resolve('docusaurus-lunr-search')],
+
   // Set the production url of your site here
   url: 'https://sd-learning.netlify.app',
   // Set the /<baseUrl>/ pathname under which your site is served
